ba39fdd75264ab0d11e79dfda681189a6c6cf016
Sasendra <Sasendra@sasendra.com>
2021-07-09 14:55:08 +0530

    docker file chanege

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("See you Youtubers ! Welcome to Software Bhayya");
+  res.send("HIIII SASENDER REDDY ! Welcome to MARS");
 });
 
 app.listen(8080, () => {
